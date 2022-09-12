b068abc57e727dfbbd9fccc6306bc8f7c1d15e3b
mstrivens <max.strivens@live.co.uk>
2022-09-12 15:52:05 +0100

    Iterating through numbers and finding most frequent then pushing into new array.

--- number_generator.py
+++ number_generator.py
@@ -14,33 +14,58 @@ def generate():
     number = random.choice(numbers)
     return f"<h1>{number}</h1>"
 
-def api_call():
+def return_best_numbers():
     balls = []
     lucky_balls = []
-    numbersArray = []
-    with requests.get(CSV_URL, stream=True) as r:
-        lines = (line.decode('utf-8') for line in r.iter_lines())
-        for row in csv.reader(lines):
-            numbersArray.append(row)
-            data = numbersArray[1: -1]
-        df = pd.DataFrame(data = data,
-                  columns = numbersArray[0])
-        # print(df)
-        for row in df.iterrows():
-            # print(row[1]['Ball 1'])
-            for header in main_balls_headers:
-                balls.append(row[1][header])
-            for header in lucky_balls_headers:
-                lucky_balls.append(row[1][header])
-                # balls.append(row[1]['Ball 1'])
-        # dataFrameRow
+    ball_counter = {}
+    with api_call(CSV_URL) as r:
+        df = return_dataframe_from_api(r)
+        extract_numbers_from_dataframe(balls, lucky_balls, df)
         print("BALLS", balls)
         print("LUCKY BALLS", lucky_balls)
-
+        for number in balls:
+            if number not in ball_counter:
+                ball_counter[number] = 1
+            else:
+                ball_counter[number] += 1
+        best_numbers = []
+        print("BALL COUNTER", ball_counter)
+        for key in ball_counter:
+            if len(best_numbers) < 6:
+                best_numbers.append(key)
+            elif ball_counter[best_numbers[5]] < ball_counter[key]:
+                best_numbers[5] = key
+            else:
+                for i in range(len(best_numbers)):
+                    if ball_counter[best_numbers[i]] < ball_counter[key]:
+                        best_numbers[i] = key
+                        break;
         table = df.to_html()
         return table
 
-api_call()
+def api_call(url):
+    return requests.get(url, stream=True)
+
+def return_dataframe_from_api(response):
+    numbers_array = []
+    lines = (line.decode('utf-8') for line in response.iter_lines())
+    for row in csv.reader(lines):
+        numbers_array.append(row)
+        data = numbers_array[1: -1]
+    return pd.DataFrame(data = data,
+              columns = numbers_array[0])
+
+def extract_numbers_from_dataframe(balls, lucky_balls, df):
+    for row in df.iterrows():
+        for header in main_balls_headers:
+            balls.append(row[1][header])
+        for header in lucky_balls_headers:
+            lucky_balls.append(row[1][header])
+
+def hash_key(hash):
+    return hash['']
+
+return_best_numbers()
 
     # text = json.dumps(response.json(), sort_keys=True, indent=4)
     # return f"<h1>{response.status_code}</h1>"
