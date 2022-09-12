8696d94fa12ee7fe82007cf252c4156f151d58c2
mstrivens <max.strivens@live.co.uk>
2022-09-12 13:32:33 +0100

    API call fetching lottery numbers csv and convering to html table using pandas and csv reader.

--- number_generator.py
+++ number_generator.py
@@ -1,11 +1,33 @@
 import random
 import requests
+import csv
+
+import pandas as pd
+
 numbers = [12, 4, 6, 83]
+
+CSV_URL = "https://www.national-lottery.co.uk/results/euromillions/draw-history/csv"
 
 def generate():
     number = random.choice(numbers)
     return f"<h1>{number}</h1>"
 
 def api_call():
-    response = requests.get("https://www.national-lottery.co.uk/results/euromillions/draw-history/csv")
-    return f"<h1>{response.status_code}</h1>"
+    numbersArray = []
+    with requests.get(CSV_URL, stream=True) as r:
+        print(csv.reader(r))
+        lines = (line.decode('utf-8') for line in r.iter_lines())
+        for row in csv.reader(lines):
+            numbersArray.append(row)
+            data = numbersArray[1: -1]
+        df = pd.DataFrame(data = data,
+                  columns = numbersArray[0])
+        # print(df)
+        table = df.to_html()
+        print(table)
+        return table
+
+api_call()
+
+    # text = json.dumps(response.json(), sort_keys=True, indent=4)
+    # return f"<h1>{response.status_code}</h1>"
