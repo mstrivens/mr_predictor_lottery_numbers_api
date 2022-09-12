37a195dc443647438150040e7177c3e68a0875a1
mstrivens <max.strivens@live.co.uk>
2022-09-12 14:51:22 +0100

    Program iterated through data frame and pulls out numbers into Arrays ready for returning most common numbers.

--- number_generator.py
+++ number_generator.py
@@ -7,15 +7,18 @@ import pandas as pd
 numbers = [12, 4, 6, 83]
 
 CSV_URL = "https://www.national-lottery.co.uk/results/euromillions/draw-history/csv"
+main_balls_headers = ['Ball 1', 'Ball 2', 'Ball 3', 'Ball 4', 'Ball 5']
+lucky_balls_headers = ['Lucky Star 1', 'Lucky Star 2']
 
 def generate():
     number = random.choice(numbers)
     return f"<h1>{number}</h1>"
 
 def api_call():
+    balls = []
+    lucky_balls = []
     numbersArray = []
     with requests.get(CSV_URL, stream=True) as r:
-        print(csv.reader(r))
         lines = (line.decode('utf-8') for line in r.iter_lines())
         for row in csv.reader(lines):
             numbersArray.append(row)
@@ -23,8 +26,18 @@ def api_call():
         df = pd.DataFrame(data = data,
                   columns = numbersArray[0])
         # print(df)
+        for row in df.iterrows():
+            # print(row[1]['Ball 1'])
+            for header in main_balls_headers:
+                balls.append(row[1][header])
+            for header in lucky_balls_headers:
+                lucky_balls.append(row[1][header])
+                # balls.append(row[1]['Ball 1'])
+        # dataFrameRow
+        print("BALLS", balls)
+        print("LUCKY BALLS", lucky_balls)
+
         table = df.to_html()
-        print(table)
         return table
 
 api_call()
